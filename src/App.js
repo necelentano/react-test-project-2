8e4ceadf5513bc05a4e0766086e6befc7ad6d56a
necelentano <necelentano@gmail.com>
2019-04-11 00:16:14 +0300

    Added routes in App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import { Route, Switch } from "react-router-dom";
 
 import Header from './components/Header/Header'
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import News from './pages/News';
+import NotFound from './pages/NotFound';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/news" component={News} />
+          <Route path="/profile" component={Profile} />
+          <Route component={NotFound} />
+        </Switch>
       </div>
     );
   }
