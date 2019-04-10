e22bdcd436f32a2dc981482d10c4c5b161ae6cdc
necelentano <necelentano@gmail.com>
2019-04-10 22:22:33 +0300

    Deleted boilerplate and added Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ToolbarLink from "./ToolbarLink/ToolbarLink";
+
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <ul>
+      <li>
+        <ToolbarLink exact to="/">
+          Home
+        </ToolbarLink>
+      </li>
+      <li>
+        <ToolbarLink to="/news">News</ToolbarLink>
+      </li>
+      <li>
+        <ToolbarLink to="/profile">Profile</ToolbarLink>
+      </li>
+    </ul>
+  );
+};
+
+export default Header;
