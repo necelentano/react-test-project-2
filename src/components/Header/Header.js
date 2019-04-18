15cec6cdeb7c1cd2946f631aea1831e73407be66
necelentano <necelentano@gmail.com>
2019-04-18 14:26:45 +0300

    Refactored from func to class and connected Header component to redux store

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,53 @@
-import React from "react";
+import React, {Component} from "react";
 import ToolbarLink from "./ToolbarLink/ToolbarLink";
+
+import { connect } from "react-redux";
+
+import { logout } from "../../store/actions/authActions";
 
 import "./Header.css";
 
-const Header = () => {
-  return (
-    <ul>
-      <li>
-        <ToolbarLink exact to="/">
-          Home
-        </ToolbarLink>
-      </li>
-      <li>
-        <ToolbarLink to="/news">News</ToolbarLink>
-      </li>
-      <li>
-        <ToolbarLink to="/profile">Profile</ToolbarLink>
-      </li>
-      <li>
-        <ToolbarLink to="/login">Login</ToolbarLink>
-      </li>
-    </ul>
-  );
+class Header extends Component {
+  onLogoutHandler = () => {
+    this.props.logout();
+  };
+
+  render() {
+    return (
+      <ul>
+        <li>
+          <ToolbarLink exact to="/">
+            Home
+          </ToolbarLink>
+        </li>
+        <li>
+          <ToolbarLink to="/news">News</ToolbarLink>
+        </li>
+        <li>
+          <ToolbarLink to="/profile">Profile</ToolbarLink>
+        </li>
+        <li>
+        {this.props.isLogged ?
+           (
+              <ToolbarLink to="/login" onClick={this.onLogoutHandler}>
+                Logot
+              </ToolbarLink>
+            ) : (
+              <ToolbarLink to="/login">Login</ToolbarLink>
+            )}
+        </li>
+      </ul>
+    );
+  }
 };
 
-export default Header;
+const mapStateToProps = state => {
+  return {
+    isLogged: state.auth.isLogged
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { logout }
+)(Header);
