f8879eea610d11fd73ff07f3e535ab5e066d9ef1
necelentano <necelentano@gmail.com>
2019-04-18 14:11:49 +0300

    Added authReducer to redux store

--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,13 @@ import thunk from "redux-thunk";
 import logger from "redux-logger";
 
 import { newsReducer } from "./reducers/newsReducer";
+import { authReducer } from "./reducers/authReducer";
 
 const middleWare = applyMiddleware(thunk, logger);
 
 const rootReducer = combineReducers({
-    news: newsReducer
+    news: newsReducer,
+    auth: authReducer
   });
 
 export const store = createStore(rootReducer, middleWare);
