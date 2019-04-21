b2728f27030d0a1ed974df5750020efbef12f3f4
necelentano <necelentano@gmail.com>
2019-04-21 20:58:39 +0300

    Edited markup in Home component.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default () => <div>Home</div>;
+const Home = () => {
+    return (
+      <div>
+        <h2>Home Page</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur facere blanditiis vero quo nostrum deserunt perferendis natus eligendi? Deleniti laboriosam, nisi suscipit ex voluptas eaque magni quod sed iste vel?</p>
+      </div>
+    );
+  };
+
+  export default Home;
 
 
