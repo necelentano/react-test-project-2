62dfe94795b0a7c28fe90c715357b6d74bb320d4
necelentano <necelentano@gmail.com>
2019-04-21 20:22:20 +0300

    Fixed authReducer

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -7,23 +7,29 @@ import {
   
   const initialState = {
     isLoging: false,
-    isLogged: false
+    isLogged: false,
+    username: ''
   };
   export const authReducer = (state = initialState, action) => {
     switch (action.type) {
       case LOGIN_REQUEST:
         return {
           isLoging: true,
-          isLogged: false
+          isLogged: false,
+          username: action.payload
         };
       case LOGIN_SUCCESS:
-        return { isLoging: false, isLogged: true };
+        return { 
+          isLoging: false, 
+          isLogged: true,
+          username: action.payload };
       case LOGIN_FAILURE:
         return {};
       case LOGOUT:
         return {
           isLoging: false,
-          isLogged: false
+          isLogged: false,
+          username: ''
         };
       default:
         return state;
