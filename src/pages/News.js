55f651e4e5f2e3a6ac9e9d3daf68ce28357fcc7d
necelentano <necelentano@gmail.com>
2019-04-22 01:45:26 +0300

    Project stucture refactored. Deleted containers/NewsContainer and connect pages/News component to redux store

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import NewsContainer from "../containers/NewsContainer";
+import NewsList from "../components/NewsList/NewsList";
+import { fetchPosts } from "../store/actions/newsActions";
 
-const News = () => {
-  return <NewsContainer />;
+class NewsContainer extends Component {
+  componentDidMount() {
+    if(this.props.posts.length === 0) {
+      this.props.fetchPosts();
+    }
+  }
+  render() {
+    return <NewsList posts={this.props.posts} />;
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    posts: state.news.posts
+  };
 };
-export default News;
+
+export default connect(
+  mapStateToProps,
+  { fetchPosts }
+)(NewsContainer);
